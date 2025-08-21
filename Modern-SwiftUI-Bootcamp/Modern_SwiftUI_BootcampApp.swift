efed0e0efcc7473fe340b36d27ea1274a17567f6
İrem Sever <iremsever_@outlook.com>
2025-08-21 22:40:10 +0300

    Initial commit

---
+++ Modern-SwiftUI-Bootcamp/Modern_SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modern_SwiftUI_BootcampApp.swift
+//  Modern-SwiftUI-Bootcamp
+//
+//  Created by İrem Sever on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Modern_SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
